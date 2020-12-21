aad14aa4bb152302f4df9116674c85da0bd19994
Wycer <wycers@gmail.com>
2020-12-21 14:58:47 +0800

    ✨ query single judgement

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.14
 
 require (
 	github.com/fatih/color v1.7.0
+	github.com/frankban/quicktest v1.11.3 // indirect
 	github.com/google/wire v0.4.0
 	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf
-	github.com/infinity-oj/server-v2 v0.0.0-20201218135102-00c69d13499f
+	github.com/infinity-oj/server-v2 v0.0.0-20201221063116-f16d9d16dea1
+	github.com/juju/go4 v0.0.0-20160222163258-40d72ab9641a // indirect
 	github.com/juju/persistent-cookiejar v0.0.0-20171026135701-d5e5a8405ef9
 	github.com/rodaine/table v1.0.1
 	github.com/spf13/viper v1.7.0
 	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20201006153459-a7d1128ccaa0 // indirect
+	gopkg.in/errgo.v1 v1.0.1 // indirect
+	gopkg.in/retry.v1 v1.0.3 // indirect
 )
--- internal/commands/judgements/query.go
+++ internal/commands/judgements/query.go
@@ -1,6 +1,7 @@
 package judgements
 
 import (
+	"fmt"
 	"github.com/infinity-oj/cli/internal/output"
 	"github.com/infinity-oj/server-v2/pkg/api"
 	"github.com/urfave/cli/v2"
@@ -19,22 +20,70 @@ func NewQueryJudgementCommand(api api.API) *cli.Command {
 		Before:       nil,
 		After:        nil,
 		Action: func(c *cli.Context) error {
-			judgements, err := api.NewJudgementAPI().Query()
-			if err != nil {
-				return err
-			}
+			judgementId := c.String("judgement")
 
-			tbl := output.NewTable("ID", "Time", "Submission", "Score")
-			for _, judgement := range judgements {
+			if judgementId == "" {
+				judgements, err := api.NewJudgementAPI().QueryJudgements()
+				if err != nil {
+					return err
+				}
+
+				tbl := output.NewTable("ID", "Time", "Submission", "Score")
+				for _, judgement := range judgements {
+					tbl.AddRow(judgement.JudgementId, judgement.CreatedAt, judgement.SubmissionId, judgement.Score)
+				}
+				tbl.Print()
+			} else {
+
+				judgement, err := api.NewJudgementAPI().QueryJudgement(judgementId)
+				if err != nil {
+					return err
+				}
+
+				tbl := output.NewTable("ID", "Time", "Submission", "Score")
 				tbl.AddRow(judgement.JudgementId, judgement.CreatedAt, judgement.SubmissionId, judgement.Score)
+				tbl.Print()
+				//
+				//var msg struct {
+				//	Warning string `json:"warning"`
+				//	Error   string `json:"error"`
+				//}
+				//
+				//err = json.Unmarshal([]byte(judgement.Msg), &msg)
+				//if err != nil {
+				//
+				//} else {
+				//	fmt.Println("Warning:")
+				//	fmt.Println(msg.Warning)
+				//	fmt.Println()
+				//	fmt.Println("Error:")
+				//	fmt.Println(msg.Error)
+				//}
+				fmt.Println()
+
+				fmt.Println(judgement.Msg)
 			}
-			tbl.Print()
 
 			return nil
 		},
-		OnUsageError:           nil,
-		Subcommands:            nil,
-		Flags:                  nil,
+		OnUsageError: nil,
+		Subcommands:  nil,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "judgement",
+				Aliases:     []string{"jid"},
+				Usage:       "",
+				EnvVars:     nil,
+				FilePath:    "",
+				Required:    false,
+				Hidden:      false,
+				TakesFile:   false,
+				Value:       "",
+				DefaultText: "",
+				Destination: nil,
+				HasBeenSet:  false,
+			},
+		},
 		SkipFlagParsing:        false,
 		HideHelp:               false,
 		Hidden:                 false,
