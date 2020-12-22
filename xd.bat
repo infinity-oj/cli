af11151243cf2112bdab6f50094952c01e802029
Wycer <wycers@gmail.com>
2020-12-22 14:06:51 +0800

    xd

---
+++ xd.bat
@@ -0,0 +1,3 @@
+scp .\dist\cli-windows-amd64.exe zhaoy@10.20.107.171:~/proj3/assets/cli
+scp .\dist\cli-darwin-amd64 zhaoy@10.20.107.171:~/proj3/assets/cli
+scp .\dist\cli-linux-amd64 zhaoy@10.20.107.171:~/proj3/assets/cli
